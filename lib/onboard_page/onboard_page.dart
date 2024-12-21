6d10a230ef49a961e1f08235375dd3c52f1166fa
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-12-21 11:06:22 +0530

    Added image assets.

---
+++ lib/onboard_page/onboard_page.dart
@@ -0,0 +1,58 @@
+
+import 'package:flutter/cupertino.dart';
+
+class OnBoardPage extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.asset("assets/images/logo.png",height: 100,width: 100,)
+      ],
+    );
+  }
+}
+
+
+class OnBoardViewOne extends StatelessWidget{
+  const OnBoardViewOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
+
+class OnBoardViewTwo extends StatelessWidget{
+  const OnBoardViewTwo({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
+
+
+
+class OnBoardViewThree extends StatelessWidget{
+  const OnBoardViewThree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
+
