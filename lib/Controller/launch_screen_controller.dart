74d5192b30a46ff802107eafce23d0d345f6a4da
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-12-22 10:45:45 +0530

    Implemented onboard view.

--- lib/Controller/launch_screen_controller.dart
+++ lib/Controller/launch_screen_controller.dart
@@ -1,8 +1,7 @@
 import 'package:get/get.dart';
 import 'package:namma_chennai/onboard_page/onboard_page.dart';
 
-class LaunchScreenController  {
-
+class LaunchScreenController {
   void navigateToOnboardingPage() {
     Future.delayed(Duration(seconds: 2)).then((onValue) {
       Get.off(() => OnBoardPage());
